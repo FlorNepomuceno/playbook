f25fd0180bd93744f20e0ded09d88f1e0d81a8a1
FlorNepomuceno <flor.esthela93@gmail.com>
2022-04-21 18:46:46 -0500

    Creando objeto vacio y añadiendo el constructor

---
+++ weekly_mission_2/example_2/example_2_1.js
@@ -0,0 +1,34 @@
+console.log("POO")
+
+//Objeto de JS
+
+const woopa_obj = {
+    name: "Woopa",
+    mision: "Node Js",
+    age: "12",
+    color: "pink"
+}
+
+
+// Crear una clase
+class Ajolonauta {
+    constructor(name, mission, age, color){
+        this.name = name
+        this.mission = mission
+        this.age = age
+        this.color = color
+    }
+
+}
+//Objeto de JS
+const woopa_obj_js = {}
+console.log(woopa_obj_js)
+
+
+const woopa = new Ajolonauta("Woopa", "Node JS", 18, "Pink")// Instanciar un objeto
+
+
+console.log(woopa)
+
+const wooper = new Ajolonauta("Wooper", "Java", 10, "Blue")
+console.log(wooper)
