0b8055781c648e24972699ac36cf85b995e75d3c
FlorNepomuceno <flor.esthela93@gmail.com>
2022-04-21 19:22:02 -0500

    Crear Metodos

--- weekly_mission_2/example_2/example_2_1.js
+++ weekly_mission_2/example_2/example_2_1.js
@@ -11,12 +11,22 @@ const woopa_obj = {
 
 
 // Crear una clase
+/*
+1.Como crear una clase
+2.Como instanciar un objeto de una clase
+3.Instanciar varios objetos de una misma clase
+4. Como agregar el constructor para guardar atributos al crear un objeto. 
+5. Crear Metodos
+*/
 class Ajolonauta {
     constructor(name, mission, age, color){
         this.name = name
         this.mission = mission
         this.age = age
         this.color = color
+    }
+    sayHello(){
+        return "Hey! Hello"
     }
 
 }
@@ -26,9 +36,9 @@ console.log(woopa_obj_js)
 
 
 const woopa = new Ajolonauta("Woopa", "Node JS", 18, "Pink")// Instanciar un objeto
-
-
 console.log(woopa)
+console.log(woopa.sayHello())
 
 const wooper = new Ajolonauta("Wooper", "Java", 10, "Blue")
 console.log(wooper)
+console.log(wooper.color)
